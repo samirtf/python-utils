4f92aa9e031ab58257ff563fc524331d4a79e287
Samir Trajano Feitosa <samircc20092@gmail.com>
2023-03-11 00:28:05 -0300

    Update replace_in_directory.py

--- replace_in_directory.py
+++ replace_in_directory.py
@@ -1,32 +1,28 @@
 import os
+import sys
 
-def replace_string_in_file(file_path, old_string, new_string):
-    with open(file_path, 'r') as f:
-        content = f.read()
-    
-    content = content.replace(old_string, new_string)
-    
-    with open(file_path, 'w') as f:
-        f.write(content)
+def replace_strings_in_file(file_path, old_string, new_string):
+    with open(file_path, 'r') as file:
+        file_contents = file.read()
+        new_contents = file_contents.replace(old_string, new_string)
+    with open(file_path, 'w') as file:
+        file.write(new_contents)
 
-def replace_file_name(file_path, old_string, new_string):
-    dir_path = os.path.dirname(file_path)
-    old_name = os.path.basename(file_path)
-    new_name = old_name.replace(old_string, new_string)
-    new_path = os.path.join(dir_path, new_name)
-    os.rename(file_path, new_path)
+def replace_strings_in_directory(directory_path, old_string, new_string):
+    for root, dirs, files in os.walk(directory_path):
+        for file in files:
+            file_path = os.path.join(root, file)
+            replace_strings_in_file(file_path, old_string, new_string)
+        for dir in dirs:
+            dir_path = os.path.join(root, dir)
+            new_dir_path = dir_path.replace(old_string, new_string)
+            os.rename(dir_path, new_dir_path)
 
-def replace_in_directory(dir_path, old_string, new_string):
-    for root, dirs, files in os.walk(dir_path):
-        for name in files:
-            file_path = os.path.join(root, name)
-            replace_string_in_file(file_path, old_string, new_string)
-            replace_file_name(file_path, old_string, new_string)
-        
-        for name in dirs:
-            dir_path = os.path.join(root, name)
-            replace_in_directory(dir_path, old_string, new_string)
-            replace_file_name(dir_path, old_string, new_string)
-
-# Example usage:
-replace_in_directory('/path/to/directory', 'old_string', 'new_string')
+if __name__ == '__main__':
+    if len(sys.argv) != 4:
+        print('Usage: python replace_strings.py <directory_path> <old_string> <new_string>')
+    else:
+        directory_path = sys.argv[1]
+        old_string = sys.argv[2]
+        new_string = sys.argv[3]
+        replace_strings_in_directory(directory_path, old_string, new_string)
