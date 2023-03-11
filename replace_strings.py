d351a558cc8496c1a232214781a54285a7c25a7a
Samir Trajano Feitosa <samircc20092@gmail.com>
2023-03-11 00:34:03 -0300

    Update replace_strings.py
    add automatic encoding detection

--- replace_strings.py
+++ replace_strings.py
@@ -1,22 +1,32 @@
 import os
 import sys
+import chardet
 
 def replace_strings_in_file(file_path, old_string, new_string):
-    with open(file_path, 'r') as file:
+    with open(file_path, 'rb') as file:
         file_contents = file.read()
+        detected_encoding = chardet.detect(file_contents)['encoding']
+    with open(file_path, 'w', encoding=detected_encoding) as file:
+        file_contents = file_contents.decode(detected_encoding)
         new_contents = file_contents.replace(old_string, new_string)
-    with open(file_path, 'w') as file:
         file.write(new_contents)
 
 def replace_strings_in_directory(directory_path, old_string, new_string):
     for root, dirs, files in os.walk(directory_path):
-        for file in files:
-            file_path = os.path.join(root, file)
+        for file_name in files:
+            file_path = os.path.join(root, file_name)
             replace_strings_in_file(file_path, old_string, new_string)
-        for dir in dirs:
-            dir_path = os.path.join(root, dir)
-            new_dir_path = dir_path.replace(old_string, new_string)
-            os.rename(dir_path, new_dir_path)
+            new_file_name = file_name.replace(old_string, new_string)
+            if new_file_name != file_name:
+                new_file_path = os.path.join(root, new_file_name)
+                os.rename(file_path, new_file_path)
+        for dir_name in dirs:
+            dir_path = os.path.join(root, dir_name)
+            new_dir_name = dir_name.replace(old_string, new_string)
+            if new_dir_name != dir_name:
+                new_dir_path = os.path.join(root, new_dir_name)
+                os.rename(dir_path, new_dir_path)
+            replace_strings_in_directory(new_dir_path, old_string, new_string)
 
 if __name__ == '__main__':
     if len(sys.argv) != 4:
